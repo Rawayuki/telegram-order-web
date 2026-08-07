906c5313eb00a7b068996982ded6e18fca3af9be
Ney <0985013899miharu@gmail.com>
2026-08-07 22:10:17 +0700

    First Commit

---
+++ app.py
@@ -0,0 +1,45 @@
+from flask import Flask, send_from_directory, request
+import requests
+
+app = Flask(__name__)
+
+TOKEN = "8834186857:AAHc3P6j64FZm972kEAwCYzF5YgSDlGfm6g"
+CHAT_ID = "8588885397"
+
+@app.route("/")
+def home():
+    return send_from_directory(".", "index.html")
+
+@app.route("/style.css")
+def style():
+    return send_from_directory(".", "style.css")
+
+@app.route("/order", methods=["POST"])
+def order():
+    data = request.get_json()
+
+    username = data.get("username", "User111")
+    product = data.get("product", "ไม่ระบุสินค้า")
+    quantity = data.get("quantity", 1)
+
+    message = f"{username} สั่ง {product} จำนวน {quantity} ชิ้น"
+
+    url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+
+    telegram_data = {
+        "chat_id": CHAT_ID,
+        "text": message
+    }
+
+    response = requests.post(url, data=telegram_data)
+
+    if response.ok:
+        return {
+            "message": f"สั่ง {product} จำนวน {quantity} ชิ้น สำเร็จ"
+        }
+
+    return {
+        "message": "ส่งคำสั่งซื้อไม่สำเร็จ"
+    }, 500
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
